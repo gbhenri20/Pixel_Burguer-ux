425a5c7eed52e408eb0ebc928b8632bef3e9c003
Gabriel Henrique <gb.henri20@gmail.com>
2026-08-25 22:55:14 +0000

    1º commit

---
+++ web2/src/App.tsx
@@ -0,0 +1,21 @@
+//import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+ 
+import './assets/lanchonete.css'
+ 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <img src={reactLogo} className="figura" alt='figura'></img>
+
+      
+    </>
+  )
+}
+
+export default App
